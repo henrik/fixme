a7f4a7fec72efacda5df8a9ea5e448365fd01a6e
Henrik Nyh <henrik@nyh.se>
2014-06-24 20:59:28 +0200

    Extract Runner class
    To enable refactoring without adding private methods to every single object.

--- lib/fixme.rb
+++ lib/fixme.rb
@@ -6,12 +6,24 @@ module Fixme
 
   module Mixin
     def FIXME(date_and_message)
+      # In a separate class to avoid mixing in privates.
+      # http://thepugautomatic.com/2014/02/private-api/
+      Runner.new(date_and_message).run
+    end
+  end
+
+  class Runner
+    def initialize(date_and_message)
+      @date_and_message = date_and_message
+    end
+
+    def run
       return if ENV["DO_NOT_RAISE_FIXMES"]
 
       env = defined?(Rails) ? Rails.env : ENV["RACK_ENV"]
       return unless [ "", "test", "development" ].include?(env.to_s)
 
-      raw_date, message = date_and_message.split(": ", 2)
+      raw_date, message = @date_and_message.split(": ", 2)
       due_date = Date.parse(raw_date)
 
       raise_if_due = -> {
